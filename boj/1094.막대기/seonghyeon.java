9f3d7b82d3b071a3448c5bf0ffdd79fa0ea2cd40
usingnameing <cnrrn0312@gmail.com>
2025-09-22 23:24:45 +0900

    Comment: 주석추가

--- boj/1094.막대기/seonghyeon.java
+++ boj/1094.막대기/seonghyeon.java
@@ -6,20 +6,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         x = sc.nextInt();
 
-        int totalLen = 64;
-        int shortestStick = 64;
-        int cnt = 1;
+        int totalLen = 64; // 막대기 길이 총 합
+        int shortestStick = 64; // 현재 가장 짧은 막대기 길이
+        int cnt = 1; // 막대기 개수
 
         while(totalLen > x){
-            int tempStickLen = shortestStick/2;
-            cnt--;
+            cnt--; // 가장 짧은 막대기 제거
+            int tempStickLen = shortestStick/2; // 새롭게 만들 가장 짧은 막대기 길이 구하기
+            shortestStick = tempStickLen; // 짧은 막대기 길이 업데이트
             if((totalLen - tempStickLen) >= x){
-                totalLen -= tempStickLen;
-                shortestStick = tempStickLen;
-                cnt+=1;
+                totalLen -= tempStickLen; // 가장 짧은 막대기 한 개 제외
+                cnt+=1; // 하나 버렸으므로 한 개만 더하기
             }else{
-                shortestStick = tempStickLen;
-                cnt+=2;
+                cnt+=2; // 버리는 막대기 없으므로 두 개 더하기
             }
         }
 
--- boj/3190.뱀/seonghyeon.java
+++ boj/3190.뱀/seonghyeon.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[][] board;
-    static Queue<Cord> directList = new LinkedList<>();
+    static int[][] board; // 뱀 : -1, 사과 : 1
+    static Queue<Cord> directList = new LinkedList<>(); // 뱡향 전환 정보 리스트
 
     static int[] dx = {0,1,0,-1};
     static int[] dy = {1,0,-1,0};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Deque<Cord> snake = new LinkedList<>();
+        Deque<Cord> snake = new LinkedList<>(); // 뱀 몸통 좌표 리스트 -> 머리 위치 추적용
         snake.add(new Cord(0,0));
 
         n = sc.nextInt();
@@ -20,11 +20,12 @@ public class Main {
         while(cnt > 0){
             int x = sc.nextInt()-1;
             int y = sc.nextInt()-1;
-            board[x][y] = 1;
+            board[x][y] = 1; // 사과 위치
             cnt--;
         }
 
         cnt = sc.nextInt();
+        // 방향 전환 정보 저장
         while(cnt>0) {
             int time = sc.nextInt();
             String direct = sc.next();
@@ -34,32 +35,32 @@ public class Main {
             }else{
                 directList.add(new Cord(time, -1));
             }
-
             cnt--;
         }
 
-        board[0][0] = -1;
+        board[0][0] = -1; // 뱀 몸통 표시
 
         cnt = 0;
         int d = 0;
         while(true){
             cnt++;
-            Cord now = snake.peek();
+            Cord now = snake.peek(); // 현재 뱀 머리 위치
+            // 다음 뱀 머리 위치
             int nx = now.x + dx[d];
             int ny = now.y + dy[d];
 
+            // 범위 밖 or 뱀 몸통 충돌 확인
             if((nx < 0 || nx >= n) || (ny < 0 || ny >= n) || board[nx][ny] == -1){
                 break;
             }
-            snake.addFirst(new Cord(nx,ny));
-            if(board[nx][ny] == 1){
-                board[nx][ny] = -1;
-            }else{
-                board[nx][ny] = -1;
+            snake.addFirst(new Cord(nx,ny)); // 머리 이동
+            if(board[nx][ny] != 1){
                 Cord tail = snake.pollLast();
                 board[tail.x][tail.y] = 0;
             }
+            board[nx][ny] = -1; // 뱀 좌표 업데이트
 
+            // 방향 전환
             if(!directList.isEmpty() && cnt == directList.peek().x){
                 Cord direct = directList.poll();
                 int direction = d + direct.y;
@@ -70,6 +71,7 @@ public class Main {
         System.out.println(cnt);
     }
 
+    // 죄표 값 클래스
     static class Cord{
         int x;
         int y;
