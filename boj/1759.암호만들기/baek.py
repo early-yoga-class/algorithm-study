e9ec7ef06fbf7c432f1e554a6cea90be994f4505
백지원 <bjiwon59@gmail.com>
2025-10-21 20:29:55 +0900

    [백지원] - 28주차 제출

--- boj/1759.암호만들기/baek.py
+++ boj/1759.암호만들기/baek.py
@@ -4,16 +4,16 @@ from itertools import combinations
 
 L, C = map(int, input().split())
 secret = sorted(list(map(str, input().split())))
-gather = set(["a", "e", "i", "o", "u"])
+vowel = set(["a", "e", "i", "o", "u"])
 
 prediction = list(combinations(secret, L))
 for check in prediction:
-    gather_count = 0
-    min_gather_count = 1
-    max_gather_count = L-2
+    vowel_count = 0
+    min_vowel_count = 1
+    max_vowel_count = L-2
     for i in range(L):
-        if check[i] in gather:
-            gather_count += 1
-    if min_gather_count <= gather_count <= max_gather_count:
+        if check[i] in vowel:
+            vowel_count += 1
+    if min_vowel_count <= vowel_count <= max_vowel_count:
         print(''.join(check))
     
