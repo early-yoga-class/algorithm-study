e50c9760063782ab3ba6b5177a8741cc89889402
백지원 <bjiwon59@gmail.com>
2026-05-11 20:48:10 +0900

    Add : 양과 늑대

---
+++ programmers/양과늑대/baek.py
@@ -0,0 +1,24 @@
+
+def solution(info, edges):
+    visited = [0] * len(info)
+    answer = []
+    
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            answer.append(sheep)
+        else:
+            return 
+        
+        for i, j in edges:
+            if visited[i] and not visited[j]:
+                visited[j] = 1
+                if info[j] == 0:
+                    dfs(sheep+1, wolf)
+                else:
+                    dfs(sheep, wolf+1)
+                visited[j] = 0
+
+    visited[0] = 1
+    dfs(1, 0)
+
+    return max(answer)
