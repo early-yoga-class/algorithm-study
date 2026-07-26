8d2d718addc8ff9a1226fadb9efeec3deac5ccef
yoominlee00 <yumini606@gmail.com>
2026-07-27 00:15:08 +0900

    modify code

--- programmers/등산코스 정하기/yoominlee.py
+++ programmers/등산코스 정하기/yoominlee.py
@@ -3,56 +3,51 @@ import heapq
 def solution(n, paths, gates, summits):
     graph = [[] for _ in range(n + 1)]
 
+    for a, b, w in paths:
+        graph[a].append((b, w))
+        graph[b].append((a, w))
+
     gate_set = set(gates)
     summit_set = set(summits)
 
-    def dijkstra(start, cost):
-        cost[start] = 0
-        heap = [(0, start)]
+    INF = float('inf')
+    intensity = [INF] * (n + 1)
 
-        while heap:
-            current_intensity, current_node = heapq.heappop(heap)
+    heap = []
 
-            if current_intensity > cost[current_node]:
+    # 모든 출입구를 시작점으로
+    for gate in gates:
+        intensity[gate] = 0
+        heapq.heappush(heap, (0, gate))
+
+    while heap:
+        current_intensity, current_node = heapq.heappop(heap)
+
+        if current_intensity > intensity[current_node]:
+            continue
+
+        # 산봉우리에 도착하면 종료
+        if current_node in summit_set:
+            continue
+
+        for next_node, edge_cost in graph[current_node]:
+
+            # 다른 출입구는 지나가지 않음
+            if next_node in gate_set:
                 continue
 
-            # 산봉우리에 도착하면 더 이동하지 않음
-            if current_node in summit_set:
-                continue
+            new_intensity = max(current_intensity, edge_cost)
 
-            for next_node, edge_cost in graph[current_node]:
-                # 다른 출입구를 중간에 지나가지 않음
-                if next_node in gate_set:
-                    continue
-
-                new_intensity = max(current_intensity, edge_cost)
-
-                if new_intensity >= cost[next_node]:
-                    continue
-
-                cost[next_node] = new_intensity
+            if new_intensity < intensity[next_node]:
+                intensity[next_node] = new_intensity
                 heapq.heappush(heap, (new_intensity, next_node))
 
-    for node_a, node_b, edge_cost in paths:
-        graph[node_a].append((node_b, edge_cost))
-        graph[node_b].append((node_a, edge_cost))
+    summits.sort()
 
-    min_summit = float('inf')
-    min_intensity = float('inf')
+    answer = [0, INF]
 
-    for gate in gates:
-        cost = [float('inf')] * (n + 1)
-        dijkstra(gate, cost)
+    for summit in summits:
+        if intensity[summit] < answer[1]:
+            answer = [summit, intensity[summit]]
 
-        for summit in summits:
-            if cost[summit] < min_intensity:
-                min_summit = summit
-                min_intensity = cost[summit]
-
-            elif (
-                cost[summit] == min_intensity
-                and summit < min_summit
-            ):
-                min_summit = summit
-
-    return [min_summit, min_intensity]
+    return answer
